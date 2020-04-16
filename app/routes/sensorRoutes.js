3b8f7c83e7b01140078d1a404bd48e6924044f62
Rashy98 <rashinikavindya@gmail.com>
2020-04-16 18:34:01 +0530

    Sensor status added ..

--- app/routes/sensorRoutes.js
+++ app/routes/sensorRoutes.js
@@ -6,7 +6,15 @@ var mysql = require("mysql");
 var dbConn = require('../models/database');
 
 module.exports = function(app) {
+
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
     app.get('/api/sensors', function (req, res) {
+
         dbConn.query('SELECT * FROM Sensors', function (error, results, fields) {
             if (error) throw error;
             return res.send({error: false, data: results, message: 'Sensor List.'});
@@ -18,12 +26,13 @@ module.exports = function(app) {
         let roomNo = req.body.roomNo;
         let smokeLevel = req.body.smokeLevel;
         let Co2Level = req.body.Co2Level;
+        let status = req.body.status;
 
-        if (!floorNo || !roomNo || !smokeLevel || !Co2Level) {
+        if (!floorNo || !roomNo || !smokeLevel || !Co2Level || !status) {
             return res.status(400).send({error: true, message: 'Please Enter sensor details'});
         }
 
-        dbConn.query("INSERT INTO Sensors SET ?",{floorNo:floorNo, roomNo:roomNo,smokeLevel:smokeLevel,Co2Level:Co2Level},function (error, results, fields) {
+        dbConn.query("INSERT INTO Sensors SET ?",{floorNo:floorNo, roomNo:roomNo,smokeLevel:smokeLevel,Co2Level:Co2Level,status:status},function (error, results, fields) {
             if (error) throw error;
             return res.send({
                 error: false,
@@ -37,11 +46,12 @@ module.exports = function(app) {
         let sensor_id = req.body.Id;
         let smokeLevel = req.body.smokeLevel;
         let Co2Level = req.body.Co2Level;
+        let status = req.body.status;
 
         if (!sensor_id||!smokeLevel || !Co2Level) {
             return res.status(400).send({ message: 'Please provide sensor details and sensor id' });
         }
-        dbConn.query("UPDATE Sensors SET smokeLevel = ?,Co2Level = ?  WHERE id = ?", [smokeLevel,Co2Level, sensor_id], function (error, results, fields) {
+        dbConn.query("UPDATE Sensors SET smokeLevel = ?,Co2Level = ? ,status =? WHERE id = ?", [smokeLevel,Co2Level,status, sensor_id], function (error, results, fields) {
             if (error) throw error;
             return res.send({ error: false, data: results, message: 'sensor has been updated successfully.' });
         });
